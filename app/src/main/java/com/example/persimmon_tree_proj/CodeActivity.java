7839cce88a317795bc46f9022e93e0ea2426eca2
s2s2bin <3dltnqls@naver.com>
2020-11-15 21:29:56 +0900

    family check first

--- app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
@@ -2,6 +2,7 @@ package com.example.persimmon_tree_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CodeActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class CodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code);
+
+        Intent intent = getIntent();
+        String str_code = intent.getStringExtra("str_code"); //family activity
     }
 }
