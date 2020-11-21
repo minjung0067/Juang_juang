bf805999c0c929a21d7dd1f384c343e78a167e25
kdmstj <rkddmstj429@gmail.com>
2020-11-21 15:29:38 +0900

    splash screen

---
+++ app/src/main/java/com/example/persimmon_tree_proj/splash.java
@@ -0,0 +1,33 @@
+package com.example.persimmon_tree_proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.Juang_juang.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),log_inactivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000); //splash screen 이 등장하는 시간
+
+    }
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
