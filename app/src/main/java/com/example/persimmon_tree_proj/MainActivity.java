f81e67a2da03a7d641202f92f13d078f5d1c2413
minjung <minjung0067@naver.com>
2020-12-01 01:38:57 +0900

    'add'

--- app/src/main/java/com/example/persimmon_tree_proj/MainActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/MainActivity.java
@@ -18,32 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn_logout;     //로그아웃 버튼
     private Button btn_goanswer; //답변하러가기 버튼
+    private Button btn_mypage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button logout = (Button)findViewById(R.id.btn_logout); //로그아웃 버튼
+        Button logout = (Button) findViewById(R.id.btn_logout); //로그아웃 버튼
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //SharedPreferences에 저장된 값들을 로그아웃 버튼을 누르면 삭제하기 위해
                 //SharedPregerences값을 불러온다.
-                Intent intent = new Intent(MainActivity.this,log_inactivity.class);
+                Intent intent = new Intent(MainActivity.this, log_inactivity.class);
                 startActivity(intent);
                 SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
                 SharedPreferences.Editor editor = auto.edit();
                 //edit.clear()는 파일 auto에 들어있는 모든 정보를 기기에서 지운다.
                 editor.clear();
                 editor.commit(); //저장
-                Toast.makeText(MainActivity.this,"로그아웃.",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "로그아웃.", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
 
-        Button goanswer = (Button)findViewById(R.id.btn_goanswer); //답변하러가기 버튼
-        goanswer.setOnClickListener(new View.OnClickListener(){
+        Button goanswer = (Button) findViewById(R.id.btn_goanswer); //답변하러가기 버튼
+        goanswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Answeactivity로 이동
@@ -52,5 +53,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button mypage = (Button) findViewById(R.id.btn_mypage); //마이페이지 버튼
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //누르면 마이페이지로 이동
+                Intent intent = new Intent(MainActivity.this, MypageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/persimmon_tree_proj/MypageActivity.java
@@ -0,0 +1,28 @@
+package com.example.persimmon_tree_proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.Juang_juang.R;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+        Button answer = (Button)findViewById(R.id.go_back);
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
