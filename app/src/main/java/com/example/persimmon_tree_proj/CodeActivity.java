d12ef5c7762f56ea65fb832d655e1f71b068388c
s2s2bin <3dltnqls@naver.com>
2020-12-20 23:41:56 +0900

    seoling plus me code activity

--- app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 package com.example.persimmon_tree_proj;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,16 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.Juang_juang.R;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-
-import static android.widget.Toast.LENGTH_LONG;
 
 
 public class CodeActivity extends AppCompatActivity {
@@ -41,37 +34,20 @@ public class CodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code); //code xml 보여주기
 
-        int familycodee = 0;
-        Intent intent = getIntent();
-        String str_code = intent.getStringExtra("str_code"); //family activity
-        Integer familycode = new Integer(familycodee);
-        mDatabase.child("users").child(String.valueOf(familycode)).child("fcode").setValue(codelist); //전체 객체를 다 쓰지 않고 하위 항목 업데이트 하는 방식으로 family code 할당하기
+        str_code = makeCode();
+        checkDatabase(str_code);
 
-
+        Toast.makeText(CodeActivity.this, "가족 코드가 생성이 완료 되었습니다.", Toast.LENGTH_SHORT).show();
+        mDatabase.removeEventListener();
+        Intent intent = new Intent(getApplicationContext(),MakeProfile.class);
+        startActivity(intent);
 
     }
 
     public String makeCode(){ //코드 만드는 함수
         tv_code = (TextView) findViewById(R.id.textView); //초기화
-    /*
-        FirebaseDatabase database = FirebaseDatabase.getInstance();//파이어베이스의 인스턴스를 가져온다 즉, root를 가져온다고 생각
-        DatabaseReference myRef =database.getReference("groups");//Root밑에있는 groups라는 위치를 참조한다
-        myRef.addValueEventListener(new ValueEventListener(){
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) { }//onDataChange
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) { }//onCancelled
-        });//addValueEventListener
-        //참조 위치에 이벤트 리스너 연결참조한 위치에 데이터가 변화가 일어날때 마다 매번 읽어온다고 생각하면된다.
-        //예를 들어 설명하면 현재 참조한 위치(myRef)가 users를 가리키고 있는데 users에 속한 데이터 중 하나라도 변경이 되면 매번 users 전체를 읽어온다.
-    */
-
-        //addchildEventListner로 변경하거나 참조할때 users childe fcode를 참조해서 가져옴 그렇게해서 if문 변경하고 else 동일한 fcode가 있을시 다시 fcode재생성으로 수정하기
-        //가족 코드만 있는 groups에서 모든 데이터 참조 그넫 모든 데이터 가져오면 너무 시간 오래걸리지 않나? 이거 나중에 효율성을 위해서 제일 윗대가리들만 가져오는거 찾아보자
         //그렇게하고 공유하기 누르면 복사 되는거 만들기
         //홈으로 가기 만들기
-        //코드 생성 후 intent로 profile만드는 xml로 이동하기
-
         for(int i=0;i<6;i++){ //총6자리 수 코드 만들기
             int randomNum =(int)(Math.random()*10); //일의 자리 수 int 값 난수 생성
             str_code += Integer.toString(randomNum);
@@ -79,11 +55,17 @@ public class CodeActivity extends AppCompatActivity {
     return str_code;
     }
 
-    public TextView checkDatabase(final String str_code) {
+    //groups라는 루트 노드 아래에 str_code 6자리를 키 값과 value로 가지는 노드 생성
+    private void writeGroupFamily(String str_code){
+        GroupFamily groupFamily = new GroupFamily(str_code);
+        mDatabase.child("groups").child(str_code).setValue(str_code);
+    }
+
+    public void checkDatabase(final String str_code) {
 
         //현재 코드가 생성하는 사용자 이름을 디바이스에 저장된 파일에서 불러오기 위함
         SharedPreferences comefile = getSharedPreferences("saveprofile", MODE_PRIVATE); // 저장된 값을 불러오기 위해 네임파일 saveprofile을 찾음
-        String name = comefile.getString("name", ""); //key에 저장된 값이 있는지 확인 없으면 ""반환
+        final String name = comefile.getString("name", ""); //key에 저장된 값이 있는지 확인 없으면 ""반환
 
         ValueEventListener valueEventListener = new ValueEventListener() {
 
@@ -101,21 +83,19 @@ public class CodeActivity extends AppCompatActivity {
                 if (tf == 0) {
                     SharedPreferences saveprofile = getSharedPreferences("saveprofile", MODE_PRIVATE); //sharedpreferences를 saveprofile이름, 기본모드로 설정함
                     SharedPreferences.Editor editor = saveprofile.edit();//저장하기 위해 editor를 이용하여 값 저장
-                    editor.putString("fcode", String.valueOf(tv_code));//코드 저장
+                    editor.putString("fcode", String.valueOf(str_code));//코드 저장
                     editor.commit(); //최종 커밋 커밋 안하면 저장 안됨
 
-                    mDatabase.child("users").child(name).child("fcode").setValue(tv_code); //user 이름 개인정보 있는 데이터 베이스에 올리기
-                    mDatabase.child("groups").setValue(tv_code); //가족 코드 별 group 으로 묶어주는 데이터 베이스에 올리기
+                    mDatabase.child("users").child(name).child("fcode").setValue(str_code); //user 이름 개인정보 있는 데이터 베이스에 올리기
+                    writeGroupFamily(str_code);//새로운 key, value 추가하는 방식으로 writeGroupFamily함수를 불러서 group에 추가함
                     tv_code.setText(str_code);//화면에 code출력하기
                 }
                 //tf ==1 같은 코드가 있음 로그에 동일한 가족 코드 발생한 정보를 띄우고 makecode다시 실행함
-                else {
+                else  {
                     Log.i("가족 코드", "동일한 가족코드가 발생하였음");
                     makeCode(); //기존 가족 코드랑 같은 값이 나왔다면 코드 생성 함수를 다시 실행하라
                 }
-
             }
-
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -129,104 +109,3 @@ public class CodeActivity extends AppCompatActivity {
     }
 
 }
-
-=======
-//package com.example.persimmon_tree_proj;
-//
-//import androidx.annotation.NonNull;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Intent;
-//import android.content.SharedPreferences;
-//import android.os.Bundle;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import com.example.Juang_juang.R;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//
-//import java.util.ArrayList;
-//
-//import static android.widget.Toast.LENGTH_LONG;
-//
-//public class CodeActivity extends AppCompatActivity {
-//    private Button bt_share;
-//    private TextView tv_code;
-//    private EditText et_introduce;
-//    private FirebaseDatabase database;
-//    private DatabaseReference mDatabase; //데이터베이스에서 데이터 읽고 쓰기위해 인스턴스 필요
-//    private String str_code;
-//}
-//
-//    public TextView makeCode(){ //코드 만드는 함수
-//        tv_code = (TextView) findViewById(R.id.textView); //초기화
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();//파이어베이스의 인스턴스를 가져온다 즉, root를 가져온다고 생각
-//        DatabaseReference myRef =database.getReference("groups");//Root밑에있는 groups라는 위치를 참조한다
-//        myRef.addValueEventListener(new ValueEventListener(){
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) { }//onDataChange
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) { }//onCancelled
-//        });//addValueEventListener
-//        //참조 위치에 이벤트 리스너 연결참조한 위치에 데이터가 변화가 일어날때 마다 매번 읽어온다고 생각하면된다.
-//        //예를 들어 설명하면 현재 참조한 위치(myRef)가 users를 가리키고 있는데 users에 속한 데이터 중 하나라도 변경이 되면 매번 users 전체를 읽어온다.
-//
-//
-//        //addchildEventListner로 변경하거나 참조할때 users childe fcode를 참조해서 가져옴 그렇게해서 if문 변경하고 else 동일한 fcode가 있을시 다시 fcode재생성으로 수정하기
-//        //가족 코드만 있는 groups에서 모든 데이터 참조 그넫 모든 데이터 가져오면 너무 시간 오래걸리지 않나? 이거 나중에 효율성을 위해서 제일 윗대가리들만 가져오는거 찾아보자
-//        //그렇게하고 공유하기 누르면 복사 되는거 만들기
-//        //홈으로 가기 만들기
-//        //코드 생성 후 intent로 profile만드는 xml로 이동하기
-//
-//
-//        mDatabase = FirebaseDatabase.getInstance().getReference();
-//        //현재 코드가 생성하는 사용자 이름을 디바이스에 저장된 파일에서 불러오기 위함
-//        SharedPreferences comefile = getSharedPreferences("saveprofile",MODE_PRIVATE);//저장된 값을 불러오기 위해 네임파일 saveprofile을 찾음
-//        String name = comefile.getString("name","");//key에 저장된 값이 있는지 확인 없으면 ""반환
-//
-//
-//        for(int i=0;i<6;i++){ //총6자리 수 코드 만들기
-//            int randomNum =(int)(Math.random()*10); //일의 자리 수 int 값 난수 생성
-//            str_code += Integer.toString(randomNum);
-//
-//
-//        }
-//
-//        if(str_code!=""){
-//            SharedPreferences saveprofile = getSharedPreferences("saveprofile", MODE_PRIVATE); //sharedpreferences를 saveprofile이름, 기본모드로 설정함
-//            SharedPreferences.Editor editor = saveprofile.edit();//저장하기 위해 editor를 이용하여 값 저장
-//            editor.putString("fcode", String.valueOf(tv_code));//코드 저장
-//            editor.commit(); //최종 커밋 커밋 안하면 저장 안됨
-//        }
-//            //파이어 베이스에 올리기
-//            mDatabase.child("users").child(name).child("fcode").setValue(tv_code); //user 이름 개인정보 있는 데이터 베이스에 올리기
-//            mDatabase.child("groups").setValue(tv_code); //가족 코드 별 group 으로 묶어주는 데이터 베이스에 올리기
-//            tv_code.setText(str_code);//화면에 code출력하기
-//
-//        return tv_code;
-//    }
-//
-//    public void share(){//공유하기
-//
-//    }
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_code); //code xml 보여주기
-//        int familycodee = 0;
-//        Intent intent = getIntent();
-//        String str_code = intent.getStringExtra("str_code"); //family activity
-//        Integer familycode = new Integer(familycodee);
-//        mDatabase.child("users").child(String.valueOf(familycode)).child("fcode").setValue(codelist); //전체 객체를 다 쓰지 않고 하위 항목 업데이트 하는 방식으로 family code 할당하기
-//
-//
-//
-//    }
-//}
->>>>>>> 73bc7b9e5705b78cb740202bb69c465aa78e370f
--- app/src/main/java/com/example/persimmon_tree_proj/GroupFamily.java
+++ app/src/main/java/com/example/persimmon_tree_proj/GroupFamily.java
@@ -1,59 +1,37 @@
-<<<<<<< HEAD
-=======
-/*package com.example.persimmon_tree_proj;
->>>>>>> 73bc7b9e5705b78cb740202bb69c465aa78e370f
 
 package com.example.persimmon_tree_proj;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
 import java.security.acl.Group;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GroupFamily {
 
-    public Integer fcode;
+    public String fcode;
+    public String str_code;
 
     public GroupFamily(){
-
+        // Default constructor required for calls to DataSnapshot.getValue(GroupFamily.class)
     }
 
-    public GroupFamily(Integer fcode){
-        this.fcode = fcode;
+    public GroupFamily(String str_code){
+        this.fcode = str_code;
     }
 
-    private void writeGroupFamily(Integer fcode){
-        GroupFamily groupFamily = new GroupFamily(fcode);
-        mDatabase.child("groups").child(fcode).setValue(group); //데이터 덮어쓰기
+    public String getFcode(){return str_code;}
+    public void setFcode(){this.fcode = str_code; }
+
+    //나는 멥으로 묶을 필요가 없는데.. 일단 적었습!
+    /*
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("fcode", str_code);
+        return result;
     }
+    */
 }
-<<<<<<< HEAD
 
-=======
-//package com.example.persimmon_tree_proj;
-//
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//
-//import java.security.acl.Group;
-//
-//public class GroupFamily {
-//
-//    public Integer fcode;
-//
-//    public GroupFamily(){
-//
-//    }
-//
-//    public GroupFamily(Integer fcode){
-//        this.fcode = fcode;
-//    }
-//        private void writeGroupFamily(Integer fcode){
-//        GroupFamily groupFamily = new GroupFamily(fcode);
-//        mDatabase.child("groups").child(fcode).setValue(group); //데이터 덮어쓰기
-//    }
-//}
-// */
->>>>>>> 6b8387e1965efeb57180d23cfe757fefe89280b0
-=======
- */
->>>>>>> 73bc7b9e5705b78cb740202bb69c465aa78e370f
 
