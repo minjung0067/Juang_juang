5018b371b56039918b796c32282344eb155bfb4d
kdmstj <rkddmstj429@gmail.com>
2020-12-21 01:26:18 +0900

    'question'

--- app/src/main/java/com/example/persimmon_tree_proj/Answeractivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Answeractivity.java
@@ -1,12 +1,13 @@
 package com.example.persimmon_tree_proj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.Juang_juang.R;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +17,7 @@ public class Answeractivity extends AppCompatActivity {
 
     private Button btn_answer;
     private EditText edit_answer;
+    private TextView textView;
     public String msg; //edit_answer의 텍스트 값을 받아 msg에 저장
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
@@ -27,6 +29,11 @@ public class Answeractivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answeractivity);
 
+        Intent intent = getIntent();
+        String question = intent.getStringExtra("question");
+
+        textView =(TextView)findViewById(R.id.txt_question2);
+        textView.setText(question);
         edit_answer = (EditText)findViewById(R.id.edit_answer);
         Button answer = (Button)findViewById(R.id.btn_answer);
         answer.setOnClickListener(new View.OnClickListener() {
