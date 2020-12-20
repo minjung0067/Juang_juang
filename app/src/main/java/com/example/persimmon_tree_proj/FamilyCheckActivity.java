be8a6f01ef77b4b3ee8c5b19c0a191dd353ed59d
minjung <minjung0067@naver.com>
2020-12-20 21:31:50 +0900

    's'

--- app/src/main/java/com/example/persimmon_tree_proj/FamilyCheckActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/FamilyCheckActivity.java
@@ -1,4 +1,4 @@
-/*package com.example.persimmon_tree_proj;
+package com.example.persimmon_tree_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -59,4 +59,3 @@ public class FamilyCheckActivity extends AppCompatActivity {
         //if(et_introduce!=null)
     }
 }
- */
