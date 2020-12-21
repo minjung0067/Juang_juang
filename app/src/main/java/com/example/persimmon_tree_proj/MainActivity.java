c57ffd8fc1e7a8e72bb7c8cd2214a34ea06de603
kdmstj <rkddmstj429@gmail.com>
2020-12-21 21:35:02 +0900

    'soobin'

--- app/src/main/java/com/example/persimmon_tree_proj/MainActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/MainActivity.java
@@ -36,16 +36,21 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_mypage;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mReference;
-    private DatabaseReference answerReference;
     private ChildEventListener mChild;
     private TextView textView; //질문 나오는 textView
-    private TextView answerView; //질문에 대한 답변이 나오는 textView
     private String question;
     private String question_position;
     private Spinner spinner; //질문 목록이 있는 spinner
     //array배열을 생성하고 spinner와 연결
     private ArrayAdapter<String> adapter;
     List<Object> Array = new ArrayList<Object>();
+
+    private FirebaseDatabase a_Database;
+    private DatabaseReference a_Reference;
+    private ChildEventListener a_Child;
+    private ListView a_View;
+    private ArrayAdapter<String> a_adapter;
+    List<Object> a_Array = new ArrayList<Object>();
 
 
 
@@ -56,20 +61,20 @@ public class MainActivity extends AppCompatActivity {
 
         textView =(TextView)findViewById(R.id.txt_question); //question 을 나타내는 textView
         spinner =(Spinner)findViewById(R.id.spinner_question); //spinner_question
-        answerView = (TextView)findViewById(R.id.txt_answer); //answer을 나타내는 textView
+        a_View = (ListView)findViewById(R.id.answer_view); //answer을 나타내는 textView
 
         initDatabase();
 
-        answerReference = mDatabase.getReference("")
+
         mReference = mDatabase.getReference("question");
         //ValueEventListener : 경로의 전체 내용에 대한 변경을 읽고 수신 대기
         mReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 adapter.clear(); //spinner에 넣을 값을 넣기 전 초기화하기
 
                 //child 내에 있는 question 데이터를 저장하는 작업
-                for(DataSnapshot questionData : datasnapshot.getChildren()){
+                for(DataSnapshot questionData : snapshot.getChildren()){
                     String question = questionData.getValue().toString();
                     Array.add(question);
                     adapter.add(question);
@@ -94,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 question = textView.getText().toString();                 //quesition이라는 변수에 문자열로 저장
                 question_position = String.valueOf(position+1);
 
+
             }
 
             @Override
@@ -101,12 +107,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
         //spinner를 연결하고, Arrayadapter와 spinner 연결
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,new ArrayList<String>());
         spinner.setAdapter(adapter);
+
+        //listView에 answer 올리기
+        a_Reference = a_Database.getReference("family");
+        a_Reference.child("family1").child("answer").child(question_position).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                a_adapter.clear(); //ListView에 넣을 값을 넣기전 초기화하기
+
+                //child 내에 있는 answer데이터를 저장하는 작업
+                for(DataSnapshot answerData : snapshot.getChildren()){
+                    String answer = answerData.getValue().toString();
+                    a_Array.add(answer);
+                    a_adapter.add(answer);
+                }
+                //ListView를 갱신하고 마지막 위치를 카운트
+                a_adapter.notifyDataSetChanged();
+                a_View.setSelection(a_adapter.getCount()-1);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        a_adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,new ArrayList<String>());
+        a_View.setAdapter(a_adapter);
+
+
+
 
         Button logout = (Button) findViewById(R.id.btn_logout); //로그아웃 버튼
         logout.setOnClickListener(new View.OnClickListener() {
@@ -150,8 +184,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void initDatabase(){
         mDatabase = FirebaseDatabase.getInstance();
+        a_Database = FirebaseDatabase.getInstance();
 
         mReference = mDatabase.getReference("question");
+        a_Reference = a_Database.getReference("family");
 
         mChild = new ChildEventListener(){
 
@@ -181,12 +217,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        a_Child = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
         mReference.addChildEventListener(mChild);
+        a_Reference.addChildEventListener(a_Child);
     }
 
     protected void onDestroy(){
         super.onDestroy();
         mReference.removeEventListener(mChild);
+        a_Reference.removeEventListener(a_Child);
     }
 }
 
