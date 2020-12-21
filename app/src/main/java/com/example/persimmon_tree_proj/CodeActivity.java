d53a4f786ea68f3e310bcac1c3bcebf3f952e0ae
s2s2bin <3dltnqls@naver.com>
2020-12-21 18:05:29 +0900

    sob log

--- app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/CodeActivity.java
@@ -20,6 +20,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class CodeActivity extends AppCompatActivity {
     private Button bt_share;
@@ -27,7 +30,7 @@ public class CodeActivity extends AppCompatActivity {
     private EditText et_introduce;
     private FirebaseDatabase database;
     private DatabaseReference mDatabase; //데이터베이스에서 데이터 읽고 쓰기위해 인스턴스 필요
-    private String str_code;
+    private String str_code = "";
     private int tf = 0;
 
     @Override
@@ -35,13 +38,14 @@ public class CodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code); //code xml 보여주기
 
+        // str_code에 6자리 숫자를 기록 할당하고 makecode안에서 checkDatabase를 돌리기 때문에 똑같은 코드가 아니면 업로드 까지
         str_code = makeCode();
-        checkDatabase(str_code);
 
-        Toast.makeText(CodeActivity.this, "가족 코드가 생성이 완료 되었습니다.", Toast.LENGTH_SHORT).show();
+        //지금 바로 만들어주고 넘어가니까 공유하기 누르면 넘어가는 걸로 바꾸기
+        //Toast.makeText(CodeActivity.this, "가족 코드가 생성이 완료 되었습니다.", Toast.LENGTH_SHORT).show();
         //mDatabase.removeEventListener();
-        Intent intent = new Intent(getApplicationContext(),MakeProfile.class);
-        startActivity(intent);
+        //Intent intent = new Intent(getApplicationContext(),MakeProfile.class);
+        //startActivity(intent);
 
     }
 
@@ -56,24 +60,26 @@ public class CodeActivity extends AppCompatActivity {
             str_code += Integer.toString(randomNum);
         }
         Toast.makeText(CodeActivity.this, "가족코드 "+ str_code +"생성 완료 ", Toast.LENGTH_SHORT).show();
+        tv_code.setText(str_code);//화면에 code출력하기
+        checkDatabase(str_code);
     return str_code;
     }
 
     //groups라는 루트 노드 아래에 str_code 6자리를 키 값과 value로 가지는 노드 생성
     private void writeGroupFamily(String str_code){
         GroupFamily groupFamily = new GroupFamily(str_code);
-        mDatabase.child("groups").child(str_code).setValue(str_code);
+        mDatabase.child(str_code).setValue(str_code);
     }
 
 
     public void checkDatabase(final String str_code) {
 
+        mDatabase = FirebaseDatabase.getInstance().getReference("groups");
         //현재 코드가 생성하는 사용자 이름을 디바이스에 저장된 파일에서 불러오기 위함
         SharedPreferences comefile = getSharedPreferences("saveprofile", MODE_PRIVATE); // 저장된 값을 불러오기 위해 네임파일 saveprofile을 찾음
         final String name = comefile.getString("name", ""); //key에 저장된 값이 있는지 확인 없으면 ""반환
-
+        Log.i("checkDatabase function","they run this fuction");
         ValueEventListener valueEventListener = new ValueEventListener() {
-
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
@@ -91,13 +97,14 @@ public class CodeActivity extends AppCompatActivity {
                     editor.putString("fcode", String.valueOf(str_code));//코드 저장
                     editor.commit(); //최종 커밋 커밋 안하면 저장 안됨
 
+                    Log.i("fucntion","data upload start");
                     mDatabase.child("users").child(name).child("fcode").setValue(str_code); //user 이름 개인정보 있는 데이터 베이스에 올리기
                     writeGroupFamily(str_code);//새로운 key, value 추가하는 방식으로 writeGroupFamily함수를 불러서 group에 추가함
                     tv_code.setText(str_code);//화면에 code출력하기
                 }
                 //tf ==1 같은 코드가 있음 로그에 동일한 가족 코드 발생한 정보를 띄우고 makecode다시 실행함
                 else  {
-                    Log.i("가족 코드", "동일한 가족코드가 발생하였음");
+                    Log.i("Check function", "same fcode");
                     makeCode(); //기존 가족 코드랑 같은 값이 나왔다면 코드 생성 함수를 다시 실행하라
                 }
             }
