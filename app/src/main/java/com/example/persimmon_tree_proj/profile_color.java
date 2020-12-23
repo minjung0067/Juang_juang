48c7ecb98d1a1b66437584fab5b9adb3819beb50
minjung <minjung0067@naver.com>
2020-12-23 23:47:40 +0900

    's'

--- app/src/main/java/com/example/persimmon_tree_proj/profile_color.java
+++ app/src/main/java/com/example/persimmon_tree_proj/profile_color.java
@@ -2,7 +2,10 @@ package com.example.persimmon_tree_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.Juang_juang.R;
 
@@ -13,4 +16,9 @@ public class profile_color extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_color);
     }
+    ImageButton goback = (ImageButton)findViewById(R.id.go_back);
+    public void onClick(View v) {
+        Intent intent = new Intent(profile_color.this, MypageActivity.class);
+        startActivity(intent);
+        }
 }
--- app/src/main/java/com/example/persimmon_tree_proj/profile_gam.java
+++ app/src/main/java/com/example/persimmon_tree_proj/profile_gam.java
@@ -2,7 +2,10 @@ package com.example.persimmon_tree_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.Juang_juang.R;
 
@@ -12,5 +15,8 @@ public class profile_gam extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_gam);
-    }
+
+        }
+        ImageButton go_back = (ImageButton)findViewById(R.id.go_back);
+
 }
