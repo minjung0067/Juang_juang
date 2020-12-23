e04c36ba72a1fe4f17e2dd6b139322aeefb3b01e
minjung <minjung0067@naver.com>
2020-12-23 23:27:31 +0900

    's'

--- app/src/main/java/com/example/persimmon_tree_proj/MypageActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/MypageActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.Juang_juang.R;
@@ -60,11 +62,19 @@ public class MypageActivity extends AppCompatActivity {
             }
         });
 
-        Button revise = (Button)findViewById(R.id.go_back);
+        ImageButton revise = (ImageButton)findViewById(R.id.edit_btn);
         revise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MypageActivity.this, MakeProfile.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton goback = (ImageButton)findViewById(R.id.go_back);
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
