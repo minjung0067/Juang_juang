66f05391a50381d62a95af4363a4960c29807be2
minjung <minjung0067@naver.com>
2020-12-27 16:02:03 +0900

    'fix_profile'

--- app/src/main/java/com/example/persimmon_tree_proj/profile_gam.java
+++ app/src/main/java/com/example/persimmon_tree_proj/profile_gam.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.Juang_juang.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,12 +22,31 @@ import com.google.firebase.database.ValueEventListener;
 
 public class profile_gam extends AppCompatActivity {
     private FirebaseAuth firebaseAuth; //파이어베이스 인증 객체 생성
+    private ImageView gam_1;
+    private ImageView gam_2;
+    private ImageView gam_3;
+    private ImageView gam_4;
+    private ImageView gam_5;
+    private ImageView gam_6;
+    private ImageView gam_7;
+    private ImageView gam_8;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_gam);
         Button color = (Button) findViewById(R.id.color);   //색상 버튼
+        gam_1 = (ImageView) findViewById(R.id.gam1);
+        gam_2 = (ImageView) findViewById(R.id.gam2);
+        gam_3 = (ImageView) findViewById(R.id.gam3);
+        gam_4 = (ImageView) findViewById(R.id.gam4);
+        gam_5 = (ImageView) findViewById(R.id.gam5);
+        gam_6 = (ImageView) findViewById(R.id.gam6);
+        gam_7 = (ImageView) findViewById(R.id.gam7);
+        gam_8 = (ImageView) findViewById(R.id.gam8);
+
         color.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +65,6 @@ public class profile_gam extends AppCompatActivity {
         });
 
     }
-
     public void Click(final View view) {  //버튼 클릭시마다 switch문으로 다른 감 프로필 선택
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -56,27 +76,35 @@ public class profile_gam extends AppCompatActivity {
                 String user_name = dataSnapshot.child("name").getValue(String.class);
                 switch (view.getId()) {
                     case R.id.gam1:
+                        gam_1.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("1");
                         break;
                     case R.id.gam2:
+                        gam_2.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("2");
                         break;
                     case R.id.gam3:
+                        gam_3.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("3");
                         break;
                     case R.id.gam4:
+                        gam_4.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("4");
                         break;
                     case R.id.gam5:
+                        gam_5.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("5");
                         break;
                     case R.id.gam6:
+                        gam_6.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("6");
                         break;
                     case R.id.gam7:
+                        gam_7.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("7");
                         break;
                     case R.id.gam8:
+                        gam_8.setBackgroundResource(R.drawable.btn_clicked);
                         FirebaseDatabase.getInstance().getReference("family").child(myfcode).child("members").child(user_name).child("user_gam").setValue("8");
                         break;
 
