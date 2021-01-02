298ac5219a3314a5677763f4c31ef4160c896dd1
minjung <minjung0067@naver.com>
2021-01-02 14:52:35 +0900

    'calendar'

--- app/src/main/java/com/example/persimmon_tree_proj/Calendar.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Calendar.java
@@ -2,7 +2,10 @@ package com.example.persimmon_tree_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.Juang_juang.R;
 
@@ -12,5 +15,24 @@ public class Calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+
+        ImageButton go_main = (ImageButton) findViewById(R.id.main_btn); //마이페이지 버튼
+        go_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //누르면 마이페이지로 이동
+                Intent intent = new Intent(Calendar.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton go_calendar = (ImageButton) findViewById(R.id.calender_btn); //마이페이지 버튼
+        go_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //누르면 마이페이지로 이동
+                Intent intent = new Intent(Calendar.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
