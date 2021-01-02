b567337ec561d3075c3e1d73449bf9127a269248
minjung <minjung0067@naver.com>
2021-01-03 02:45:19 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/Calendar.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Calendar.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.Juang_juang.R;
 
@@ -16,6 +18,19 @@ public class Calendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+
+        // imageView가 추가될 linearLayout
+        LinearLayout layout = (LinearLayout)findViewById(R.id.calender);
+
+        // layout param 생성
+        LinearLayout.LayoutParams layoutParams =
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT , LinearLayout.LayoutParams.WRAP_CONTENT , 1f /* layout_weight */);
+
+
+        ImageView iv = new ImageView(this);  // 새로 추가할 imageView 생성
+        iv.setImageResource(R.drawable.image);  // imageView에 내용 추가
+
+        layout.addView(iv); // 기존 linearLayout에 imageView 추가
 
         ImageButton go_main = (ImageButton) findViewById(R.id.main_btn); //왔다감 버튼
         go_main.setOnClickListener(new View.OnClickListener() {
