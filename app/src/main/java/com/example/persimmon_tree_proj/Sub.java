7897edb6519141669d758c68638db6b120cf89b1
minjung <minjung0067@naver.com>
2021-01-04 11:20:40 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/Sub.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Sub.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import com.example.Juang_juang.R;
 
-
+//캘린더 페이지 속 모든 가족 감 프로필 불러오기 위해 동적으로 생성하는 레이아웃
 public class Sub extends LinearLayout{
 
         public Sub(Context context, AttributeSet attrs) {
