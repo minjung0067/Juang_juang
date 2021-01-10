387ae0ed95c072cabc8f46d56c55e80401b7da3c
kdmstj <rkddmstj429@gmail.com>
2021-01-10 14:55:32 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/UserModel.java
+++ app/src/main/java/com/example/persimmon_tree_proj/UserModel.java
@@ -4,18 +4,16 @@ public class UserModel {
     //사용자의 정보가 담기는 model
     public String name; //사용자 이름
     public String uid; //현재 로그인한 사용자
-    public String phone;
     public String birth;
     public String fcode;
     public String introduce;
 
-    public UserModel(String name, String uid, String birth) {
+    public UserModel() {
 
     }
-    public UserModel(String name, String uid, String phone, String birth){
+    public UserModel(String name, String uid, String birth){
         this.name = name;
         this.uid = uid;
-        this.phone = phone;
         this.birth = birth;
         this.fcode = fcode;
         this.introduce = introduce;
@@ -29,15 +27,6 @@ public class UserModel {
     public void setUserName(String name) {
         this.name = name;
     }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
 
     public String getBirth() {
         return birth;
@@ -59,11 +48,9 @@ public class UserModel {
         return "User{" +
                 "userName='" + name + '\'' +
                 ", uid ='" + uid + '\'' +
-                ", phone='" + phone + '\'' +
                 ", birth='" + birth + '\'' +
                 ", family code='" + fcode + '\''+
                 ", introduce my self ='" + introduce +'\''+
                 '}';
     }
 }
-
