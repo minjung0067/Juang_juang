761c0e7fb182b51038e08c53232953ee5f33c7fd
kdmstj <rkddmstj429@gmail.com>
2021-01-17 23:36:23 +0900

    's'

--- app/src/main/java/com/example/persimmon_tree_proj/ShareCalendarActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/ShareCalendarActivity.java
@@ -157,16 +157,12 @@ public class ShareCalendarActivity extends Activity implements OnItemClickListen
             }
         });
         //설정 버튼
-        ImageButton go_setting = (ImageButton) findViewById(R.id.btn_mypage);
-        go_setting.setOnClickListener(new View.OnClickListener() {
+        ImageButton go_mypage = (ImageButton) findViewById(R.id.btn_mypage);
+        go_mypage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { //누르면 캘린더로 이동
-<<<<<<< HEAD
                 Intent intent = new Intent(getApplicationContext(),MypageActivity_cal.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-=======
-                Intent intent = new Intent(ShareCalendarActivity.this,customer_sound.class);
->>>>>>> 5d794d0515e18273065cc1d15e591d77f2f7c835
                 intent.putExtra("f_code",f_code);
                 startActivity(intent);
                 finish();
