11740225e673e17e11f033e49579a5627e59f5fe
minjung <minjung0067@naver.com>
2021-01-20 20:36:02 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/Popup_Cal/PopupcalActivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Popup_Cal/PopupcalActivity.java
@@ -132,12 +132,12 @@ public class PopupcalActivity extends Activity {
                           private void createMenu1(SwipeMenu menu) {
                               SwipeMenuItem item1 = new SwipeMenuItem(getApplicationContext());
                               item1.setBackground(new ColorDrawable(Color.rgb(255,255,255)));
-                              item1.setWidth((210));
+                              item1.setWidth((190));
                               item1.setIcon(R.drawable.revise_icon_2);
                               menu.addMenuItem(item1);
                               SwipeMenuItem item2 = new SwipeMenuItem(getApplicationContext());
                               item2.setBackground(new ColorDrawable(Color.rgb(255,255,255)));
-                              item2.setWidth(210);
+                              item2.setWidth(190);
                               item2.setIcon(R.drawable.delete_icon_2);
                               menu.addMenuItem(item2);
                           }
