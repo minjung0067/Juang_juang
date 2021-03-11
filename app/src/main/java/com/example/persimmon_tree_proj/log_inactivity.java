5bb0912c643286102101e72ca2a411262c00c5d7
minjung <minjung0067@naver.com>
2021-03-11 19:16:40 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/log_inactivity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/log_inactivity.java
@@ -43,23 +43,13 @@ import com.nhn.android.naverlogin.OAuthLoginHandler;
 public class log_inactivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth; //파이어베이스 인증 객체 생성
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
-    private String loginId; //자동 로그인 아이디
-    private String loginPwd; //자동 로그인 비밀번호
-    private String loginUid; //자동 로그인 비밀번호
     private Button buttonSignUp; //회원가입 버튼
     // 구글로그인 result 상수
     private static final int RC_SIGN_IN = 900; //RC_SIGN_IN 상수는 구글로그인 버튼을 클릭하여 startactivityforresult 응답 코드로 사용
-    // 구글api클라이언트
+    // 구글api 클라이언트
     private GoogleSignInClient googleSignInClient;
-    // 구글  로그인 버튼
+    // 구글 로그인 버튼
     private SignInButton buttonGoogle;
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mReference;
-    private String myfcode;
-    private String introduce;
-    private String myfam_count;
-    private String myfam_introduce;
-
 
     //네이버로그인
     ImageView ll_naver_login;
@@ -83,29 +73,35 @@ public class log_inactivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_inactivity);
 
-        //카카오톡 로그인
+        //카카오톡 로그인 버튼 선언
         kakao_login = (com.kakao.usermgmt.LoginButton) findViewById(R.id.btn_kakao_login);
 
+
+        //카카오톡 로그인 버튼을 눌렀을 때
         kakao_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 session.open(AuthType.KAKAO_LOGIN_ALL, log_inactivity.this);
             }
         });
+        //카카오톡 로그인 끝
 
 
 
 
+        //파이어베이스 가져오기
         firebaseAuth = FirebaseAuth.getInstance();
-        buttonGoogle = findViewById(R.id.btn_googleSignIn); //구글 로그인 버튼
+
+        //구글 로그인 버튼
+        buttonGoogle = findViewById(R.id.btn_googleSignIn);
 
         //google 로그인 버튼 텍스트 바꾸기
         TextView textView = (TextView) buttonGoogle.getChildAt(0);
         textView.setText("Google signin");
 
 
-
-        buttonSignUp = (Button) findViewById(R.id.btn_signup);  //회원가입으로 연결하는 버튼
+        //이메일로 시작하기 버튼을 눌렀을 때
+        buttonSignUp = (Button) findViewById(R.id.btn_signup);
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,165 +109,7 @@ public class log_inactivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        SharedPreferences auto = getSharedPreferences("auto", AppCompatActivity.MODE_PRIVATE);
-        final SharedPreferences.Editor autoLogin = auto.edit();
-        //자동로그인을 위한 파일명 auto SharedPreference 선언
-        loginId = auto.getString("inputId", null);
-        loginPwd = auto.getString("inputPwd", null);
-        loginUid = auto.getString("inputUid", null);
-        //키 값은 자유, 값은 null
-        //login된 값(설정값을)저장하기 위한 변수
-
-        //검사하면서 자동로그인!!!!!!
-        FirebaseUser user = firebaseAuth.getCurrentUser();    //파이어베이스에서 user 가져와서
-        if (FirebaseAuth.getInstance().getCurrentUser() != null){  //아예 생전 로그인 안해본 애면 바로 넘어가게 함
-            if (user.getUid().equals(loginUid) && (loginId != null)) {
-                //한번 로그인한 적 있고
-                //log_inaxtivity로 들어왔을 때 loginID와 loginPwd값을 가져와서 null이 아니라면,
-                //현재 로그인한 user uid로 접근해서 fcode 랑 한줄 소개 있는 애만 자동로그인 되게
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");//users에서 현 uid 가진 사람 찾기
-                reference.child(loginUid).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        myfcode = dataSnapshot.child("fcode").getValue(String.class);
-                        introduce = dataSnapshot.child("introduce").getValue(String.class);
-                        firebaseAuth = FirebaseAuth.getInstance();
-                        FirebaseUser user = firebaseAuth.getCurrentUser();
-                        final DatabaseReference reference2 = FirebaseDatabase.getInstance().getReference("family");
-                        reference2.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                //myfam_count = dataSnapshot.child(myfcode).child("count").getValue(String.class);
-                                //myfam_introduce = dataSnapshot.child(myfcode).child("family_name").getValue(String.class);
-                                if (myfcode == null) {//코드가 없으면
-                                    Intent intent = new Intent(getApplicationContext(),familyactivity.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                    startActivity(intent);
-                                    Toast.makeText(log_inactivity.this, "아직 가족코드가 없어요!", Toast.LENGTH_SHORT).show();
-                                    finish();
-                                } else { //코드 있으면
-                                    myfam_count = dataSnapshot.child(myfcode).child("count").getValue(String.class);
-                                    myfam_introduce = dataSnapshot.child(myfcode).child("family_name").getValue(String.class);
-                                    if (myfam_introduce==null || myfam_count.equals("0")==true){ //코드 만드는 사람이 아예 가족 프로필 안 만들었으면
-                                        Intent intenttt = new Intent(getApplicationContext(), Make_FamilyProfile.class);
-                                        intenttt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        startActivity(intenttt);
-                                        Toast.makeText(log_inactivity.this, "아직 가족 프로필을 만들지 않았어요!!", Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    }
-                                    else{
-                                        if (myfam_count.equals("")==true || myfam_introduce.equals("")==true){ //코드 만든 사람이 가족 프로필 안 만들었는데 다른 사람이 코드 치고 들어온 경우
-                                            Intent intenttt = new Intent(getApplicationContext(), Make_FamilyProfile.class);
-                                            intenttt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                            startActivity(intenttt);
-                                            Toast.makeText(log_inactivity.this, "아직 가족 프로필을 만들지 않았어요!!", Toast.LENGTH_SHORT).show();
-                                            finish();
-                                        }
-                                        else{
-                                            if (introduce == null) {//한줄 소개 없으면
-                                                Intent intentt = new Intent(getApplicationContext(), MakeProfile.class);
-                                                intentt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                                startActivity(intentt);
-                                                Toast.makeText(log_inactivity.this, "아직 소개를 입력하지 않았어요!", Toast.LENGTH_SHORT).show();
-                                                finish();
-                                            } else { //한줄소개까지 있으면
-                                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                                //자동로그인이 되었다면, Mainactivity로 바로 이동
-                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                                startActivity(intent);
-                                                Toast.makeText(log_inactivity.this, "자동로그인 성공.", Toast.LENGTH_SHORT).show();
-                                                finish();
-                                            }
-                                        }
-
-                                    }
-                                }
-                            }
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-                                throw databaseError.toException();
-                            }
-                        });
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        throw databaseError.toException();
-                    }
-                });
-
-            }
-        }
-
-
-        /*buttonLogIn = (Button) findViewById(R.id.btn_login);   //로그인 버튼
-        buttonLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {   //둘 다 비어있지 않으면
-                    loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
-                    //inputId와 inputPwd에 이메일, 비밀번호 저장
-                    autoLogin.putString("inputId", editTextEmail.getText().toString());
-                    autoLogin.putString("inputPwd", editTextPassword.getText().toString());
-                    if(user != null){
-                        autoLogin.putString("inputUid", firebaseAuth.getCurrentUser().getUid());
-                        autoLogin.commit(); //값 저장
-                    }
-                    else{
-
-                    }
-
-
-                    firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
-                        @Override
-                        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {  //로그인 되는 경우 MainActivity로 이동
-                            FirebaseUser user = firebaseAuth.getCurrentUser();    //파이어베이스에서 user 가져와서
-                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");//users에서 현 uid 가진 사람 찾기
-                            reference.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    myfcode = dataSnapshot.child("fcode").getValue(String.class);
-                                    introduce = dataSnapshot.child("introduce").getValue(String.class);
-                                    if (myfcode == null) {//코드가 없으면
-                                        Intent intentt = new Intent(getApplicationContext(), familyactivity.class);
-                                        intentt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        startActivity(intentt);
-                                        finish();
-                                    } else { //코드 있으면
-                                        if (introduce == null) {//한줄 소개 없으면
-                                            Intent intentt = new Intent(getApplicationContext(), MakeProfile.class);
-                                            intentt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                            startActivity(intentt);
-                                            finish();
-                                        } else { //한줄소개까지 있으면
-                                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                            //자동로그인이 되었다면, Mainactivity로 바로 이동
-                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                            startActivity(intent);
-                                            finish();
-                                        }
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-                                    throw databaseError.toException();
-                                }
-                            });
-
-
-                        }
-                    };
-
-                } else {   //아니라면
-                    Toast.makeText(log_inactivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();   //입력하라고 토스트 띄움
-                }
-            }
-        });
-        */
+        //이메일로 시작하기 버튼 끝
 
 
         // Google 로그인을 앱에 통합
@@ -295,6 +133,8 @@ public class log_inactivity extends AppCompatActivity {
 
         mContext = getApplicationContext();
 
+
+        //네이버 로그인
         ll_naver_login = findViewById(R.id.naverlogin);
         btn_logout = findViewById(R.id.btn_logout);
 
@@ -308,7 +148,7 @@ public class log_inactivity extends AppCompatActivity {
                         ,getString(R.string.naver_client_secret)
                         ,getString(R.string.naver_client_name)
                         //,OAUTH_CALLBACK_INTENT
-                        // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
+                        // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다. -> 네...
                 );
 
                 @SuppressLint("HandlerLeak")
@@ -343,24 +183,7 @@ public class log_inactivity extends AppCompatActivity {
 
     }
 
-    /*public void loginUser(String email, String password) {
-        firebaseAuth.signInWithEmailAndPassword(email, password)   //이메일 패스워드 방식으로 로그인하는 함수
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // 로그인 성공
-                            Toast.makeText(log_inactivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            firebaseAuth.addAuthStateListener(firebaseAuthListener);   //파이어베이스에 사용자 추가
-                        } else {
-                            // 로그인 실패
-                            Toast.makeText(log_inactivity.this, "아이디 또는 비밀번호를 다시 한번 확인해달라감!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-     */
+    //네이버 로그인은 여기까지
 
     @Override
     protected void onStart() {
@@ -378,7 +201,7 @@ public class log_inactivity extends AppCompatActivity {
 
 
     // 사용자가 정상적으로 로그인한 후에 GoogleSignInAccount 개체에서 ID 토큰을 가져와서
-    // Firebase 사용자 인증 정보로 교환하고 Firebase 사용자 인증 정보를 사용해 Firebase에 인증어쩌구,,, 한다
+    // Firebase 사용자 인증 정보로 교환하고 Firebase 사용자 인증 정보를 사용해 Firebase를 통해 인증한다
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);   //id 토큰 가져와서
@@ -390,12 +213,15 @@ public class log_inactivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // 구글로그인 성공
                             Toast.makeText(log_inactivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(log_inactivity.this, MainActivity.class);  //구글 로그인 성공시 familyactivity로 넘어가게
+                            Intent intent = new Intent(log_inactivity.this, familyactivity.class);  //구글 로그인 성공시 familyactivity로 넘어가게
                             startActivity(intent);
                             finish();
                         } else {
                             // 구글로그인 실패
                             Toast.makeText(log_inactivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(log_inactivity.this, familyactivity.class);  //구글 로그인 성공시 familyactivity로 넘어가게
+                            startActivity(intent);
+                            finish();
 
                         }
 
