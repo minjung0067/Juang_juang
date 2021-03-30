70786c154232b66b0d0e0b178b8e574da6c2fbaf
minjung <minjung0067@naver.com>
2021-03-31 00:23:18 +0900

    'fix'

--- app/src/main/java/com/example/persimmon_tree_proj/Game_activity.java
+++ app/src/main/java/com/example/persimmon_tree_proj/Game_activity.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.Juang_juang.R;
+import com.example.persimmon_tree_proj.Calendar.ShareCalendarActivity;
 import com.example.persimmon_tree_proj.Main.MainActivity;
+import com.example.persimmon_tree_proj.Mypage.MypageActivity;
+import com.example.persimmon_tree_proj.QNA.QNA_Activity;
+import com.example.persimmon_tree_proj.To_do_list.Todolist_Activity;
 
 public class Game_activity extends AppCompatActivity {
 
@@ -34,5 +38,87 @@ public class Game_activity extends AppCompatActivity {
                 startActivity(intentt);
             }
         });
+
+
+        //마이페이지 버튼
+        ImageButton mypage = (ImageButton) findViewById(R.id.btn_mypage);
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentt = new Intent(getApplicationContext(), MypageActivity.class);
+                intentt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intentt.putExtra("f_code",f_code);
+                startActivity(intentt);
+            }
+        });
+
+
+        //왔다감 버튼
+        ImageButton go_qna = (ImageButton) findViewById(R.id.qna_btn);
+        go_qna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), QNA_Activity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("f_code",f_code);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //intent시 효과 없애기
+                finish();
+            }
+        });
+
+        //공유캘린더 버튼
+        ImageButton go_calendar = (ImageButton) findViewById(R.id.calendar_btn);
+        go_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //누르면 캘린더로 이동
+                Intent intent = new Intent(getApplicationContext(), ShareCalendarActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("f_code",f_code);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //intent시 효과 없애기
+            }
+        });
+
+        //왔다감 버튼
+        ImageButton go_main = (ImageButton) findViewById(R.id.main_btn);
+        go_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("f_code",f_code);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //intent시 효과 없애기
+                finish();
+            }
+        });
+
+        //뭐할감
+        ImageButton go_todo = (ImageButton) findViewById(R.id.to_do_btn);
+        go_todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Todolist_Activity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("f_code",f_code);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //intent시 효과 없애기
+            }
+        });
+
+        //게임
+        ImageButton go_game = (ImageButton) findViewById(R.id.game_btn);
+        go_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Game_activity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("f_code",f_code);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //intent시 효과 없애기
+            }
+        });
+
     }
 }
