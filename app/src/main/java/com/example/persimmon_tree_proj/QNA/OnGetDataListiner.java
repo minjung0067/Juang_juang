a4c43aab8a31fd139f4ea7e387173f615690133a
sS2S2bin <52414079+sS2S2bin@users.noreply.github.com>
2021-04-13 20:16:54 +0900

    using interface but failed

---
+++ app/src/main/java/com/example/persimmon_tree_proj/QNA/OnGetDataListiner.java
@@ -0,0 +1,14 @@
+package com.example.persimmon_tree_proj.QNA;
+
+import com.google.firebase.database.DataSnapshot;
+
+public interface OnGetDataListiner {
+    void onSuccess(DataSnapshot dataSnapshot);
+
+    //    void onCallback(String value);
+    //void onSuccess(DataSnapshot dataSnapshot);
+    void onStart();
+    void onFailure();
+
+    void onSuccess(Object value);
+}
